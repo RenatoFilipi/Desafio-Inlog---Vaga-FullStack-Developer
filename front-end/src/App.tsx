c2191d72635ea677ca2b39b96b1bcb444d454c9b
Renato <re.rosa98@gmail.com>
2025-08-21 22:09:08 -0300

    wip

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div className="flex justify-center items-center w-full h-dvh">Hello world</div>;
+  return (
+    <div className="flex justify-center items-center w-full h-dvh">
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default App;
