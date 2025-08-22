94ca7b5b12b315e86fc8af2ab9ad252afa2ddd5e
Renato <re.rosa98@gmail.com>
2025-08-22 00:11:22 -0300

    chore: inicio do tab

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,71 +1,120 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import type { Vehicle } from "@/lib/interfaces";
-import type { appState } from "@/lib/utils";
-import { CarIcon, Loader2Icon, MapPinIcon, PlusIcon, XCircleIcon } from "lucide-react";
+import { haversineDistance, type appState } from "@/lib/utils";
+import { BirdIcon, CarIcon, Loader2Icon, MapPinIcon, PlusIcon, XCircleIcon } from "lucide-react";
 import { useEffect, useState } from "react";
+
+type tabType = "vehicle-list" | "map";
+
+const tabs: { label: string; type: tabType }[] = [
+  { label: "Lista de veículos", type: "vehicle-list" },
+  { label: "Mapa", type: "map" },
+];
 
 const Home = () => {
   const [appState, setAppState] = useState<appState>("loading");
-  const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+  const [vehicles, setVehicles] = useState<(Vehicle & { distance?: number })[]>([]);
+  const [currentTab, setCurrentTab] = useState<tabType>("vehicle-list");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setAppState("loading");
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          const userLat = position.coords.latitude;
+          const userLon = position.coords.longitude;
+          const response = await fetch("https://localhost:7222/api/Veiculo/Listar");
+          if (!response.ok) throw new Error("");
 
-        const response = await fetch("https://localhost:7222/api/Veiculo/Listar");
-        if (!response.ok) throw new Error("");
-        const jsonData = (await response.json()) as Vehicle[];
-        setVehicles(jsonData);
-        setAppState("idle");
+          const jsonData = (await response.json()) as Vehicle[];
+
+          const withDistance = jsonData.map((v) => ({
+            ...v,
+            distance: haversineDistance(userLat, userLon, v.latitude, v.longitude),
+          }));
+
+          const sorted = withDistance.sort((a, b) => (a.distance ?? 0) - (b.distance ?? 0));
+
+          setVehicles(sorted);
+          setAppState("idle");
+        });
       } catch (error) {
         console.log(error);
         setAppState("error");
       }
     };
+
     fetchData();
   }, []);
 
   return (
     <div className="flex flex-col justify-start items-center w-full min-h-dvh py-10 gap-10">
-      <div className="w-full flex justify-between items-center gap-3 sm:max-w-7xl px-4 flex-col sm:flex-row">
-        <div className="flex flex-col gap-1 w-full sm:w-fit">
-          <h1 className="font-bold text-xl">Carros cadastrados</h1>
-          <p className="text-sm text-muted-foreground">Listagem dos carros cadastrados na nossa base de dados</p>
-        </div>
-        <Button className="w-full sm:w-fit">
-          <PlusIcon />
-          Cadastrar Novo Carro
-        </Button>
-      </div>
-      <div className="sm:max-w-7xl px-4 w-full">
-        {appState === "error" && (
-          <div className="flex justify-center items-center w-full h-72 flex-col gap-2">
-            <XCircleIcon className="text-foreground w-8 h-8" />
-            <span className="text-sm text-muted-foreground">Erro na busca dos carros cadastrados</span>
+      <div className="flex flex-col w-full gap-12 sm:max-w-7xl px-4 sm:px-0">
+        <div className="flex flex-col gap-4">
+          <div className="w-full flex justify-between items-center gap-3 flex-col sm:flex-row">
+            <div className="flex flex-col gap-1 w-full sm:w-fit">
+              <h1 className="font-bold text-xl">Carros cadastrados</h1>
+              <p className="text-sm text-muted-foreground">Listagem dos carros cadastrados na nossa base de dados</p>
+            </div>
+            <Button className="w-full sm:w-fit">
+              <PlusIcon />
+              Cadastrar Novo Carro
+            </Button>
           </div>
-        )}
-        {appState === "loading" && (
-          <div className="flex justify-center items-center w-full h-72 flex-col gap-2">
-            <Loader2Icon className="text-primary w-8 h-8 animate-spin" />
-            <span className="text-sm text-muted-foreground">Buscando os carros cadastrados...</span>
-          </div>
-        )}
-        {appState === "idle" && vehicles.length <= 0 && (
-          <Card className="flex justify-center items-center w-full h-72 flex-col bg-foreground/5 p-5 gap-4">
-            <CarIcon className="text-primary w-8 h-8" />
-            <p className="text-sm text-muted-foreground text-center">
-              Nenhum carro cadastrado na base de dados, cadastre para ver na lista.
-            </p>
-          </Card>
-        )}
-        {appState === "idle" && vehicles.length > 0 && (
-          <div className="flex flex-col w-full gap-6">
-            {vehicles.map((vehicle) => {
-              return <VehicleCard key={vehicle.chassi} vehicle={vehicle} />;
+          <div className="flex border-b w-full">
+            {tabs.map((tab) => {
+              const isCurrentTab = currentTab === tab.type;
+              return (
+                <button
+                  onClick={() => setCurrentTab(tab.type)}
+                  key={tab.type}
+                  className={`${
+                    isCurrentTab ? "text-primary" : "text-foreground/70 hover:text-foreground"
+                  } relative px-4 py-2 text-sm font-medium cursor-pointer`}>
+                  {tab.label}
+                  {isCurrentTab && <span className="absolute bottom-0 left-0 w-full h-0.5 bg-primary rounded-t" />}
+                </button>
+              );
             })}
           </div>
+        </div>
+        {currentTab === "vehicle-list" && (
+          <div className="w-full">
+            {appState === "error" && (
+              <div className="flex justify-center items-center w-full h-72 flex-col gap-2">
+                <XCircleIcon className="text-foreground w-8 h-8" />
+                <span className="text-sm text-muted-foreground">Erro na busca dos carros cadastrados</span>
+              </div>
+            )}
+            {appState === "loading" && (
+              <div className="flex justify-center items-center w-full h-72 flex-col gap-2">
+                <Loader2Icon className="text-primary w-8 h-8 animate-spin" />
+                <span className="text-sm text-muted-foreground">Buscando os carros cadastrados...</span>
+              </div>
+            )}
+            {appState === "idle" && vehicles.length <= 0 && (
+              <Card className="flex justify-center items-center w-full h-72 flex-col bg-foreground/5 p-5 gap-4">
+                <CarIcon className="text-primary w-8 h-8" />
+                <p className="text-sm text-muted-foreground text-center">
+                  Nenhum carro cadastrado na base de dados, cadastre para ver na lista.
+                </p>
+              </Card>
+            )}
+            {appState === "idle" && vehicles.length > 0 && (
+              <div className="flex flex-col w-full gap-6">
+                {vehicles.map((vehicle) => {
+                  return <VehicleCard key={vehicle.chassi} vehicle={vehicle} />;
+                })}
+              </div>
+            )}
+          </div>
+        )}
+        {currentTab === "map" && (
+          <Card className="flex justify-center items-center w-full h-72 flex-col gap-3">
+            <BirdIcon className="text-primary" />
+            <span>Trabalho em progresso.</span>
+          </Card>
         )}
       </div>
     </div>
